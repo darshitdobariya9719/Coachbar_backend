c413c19716a61ebf79a601fc168f594f7ee260db
darshit.dobariya@cybercom.in <darshit.dobariya@cybercom.in>
2025-03-19 18:17:57 +0530

    cors issue fix

--- app.js
+++ app.js
@@ -25,6 +25,19 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204); // Respond OK to preflight requests
+  }
+
+  next();
+});
+
 
 app.use(json());
 
