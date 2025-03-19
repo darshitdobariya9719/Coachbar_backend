ee94c0e1e63177e7d1c1421fc4f66cf10d2817fb
darshit.dobariya@cybercom.in <darshit.dobariya@cybercom.in>
2025-03-19 17:54:20 +0530

    add corse

--- app.js
+++ app.js
@@ -9,13 +9,22 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
+// app.use(
+//   cors({
+//     origin: "*", // Allows all origins (change this to specific origins if needed)
+//     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+//     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+//   })
+// );
 app.use(
   cors({
     origin: "*", // Allows all origins (change this to specific origins if needed)
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+    credentials: true // Allow credentials (cookies, authorization headers, etc.)
   })
 );
+
 app.use(json());
 
 connectDB();
