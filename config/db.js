23b2a297d26327a2282206a3499637776399513b
darshit.dobariya@cybercom.in <darshit.dobariya@cybercom.in>
2025-03-19 19:17:34 +0530

    fix db url

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 const connectDB = async () => {
   try {
     console.log("MongoDB connecting...");
-    await connect(`mongodb+srv://darshitdobariya9719:Darshit%402021@demo.imf2a.mongodb.net/?retryWrites=true&w=majority&appName=demo/test`);
+    await connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("MongoDB connection error:", err);
