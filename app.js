d95e51cc4e9df078ea56c5d8407cfd5908feb63a
darshit.dobariya@cybercom.in <darshit.dobariya@cybercom.in>
2025-03-19 18:14:05 +0530

    add origines

--- app.js
+++ app.js
@@ -9,11 +9,19 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
+// app.use(
+//   cors({
+//     origin: "*", // Allows all origins (change this to specific origins if needed)
+//     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+//     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+//   })
+// );
 app.use(
   cors({
-    origin: "*", // Allows all origins (change this to specific origins if needed)
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+    origin: process.env.CLIENT_URL, // Allow only your frontend
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true // Allow cookies or authorization headers
   })
 );
 
