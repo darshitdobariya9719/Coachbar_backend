c21efa16b7850058d14bedb900d034ec4fa52ff5
darshit.dobariya@cybercom.in <darshit.dobariya@cybercom.in>
2025-03-19 19:06:59 +0530

    change connection string

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 const connectDB = async () => {
   try {
     console.log("MongoDB connecting...",`${process.env.DB_URL}/${process.env.DB_NAME}`);
-    await connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@<cluster>.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`);
+    await connect(`mongodb+srv://darshitdobariya9719:Darshit%402021@demo.imf2a.mongodb.net/?appName=demo/productmanagement`);
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("MongoDB connection error:", err);
