cb353d815fc2704536ea03435e3f464db6bbe32c
darshit.dobariya@cybercom.in <darshit.dobariya@cybercom.in>
2025-03-19 18:36:37 +0530

    add log

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ import { connect } from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log("MongoDB connecting...",`${process.env.DB_URL}/${process.env.DB_NAME}`);
     await connect(`${process.env.DB_URL}/${process.env.DB_NAME}`);
     console.log("MongoDB connected successfully");
   } catch (err) {
