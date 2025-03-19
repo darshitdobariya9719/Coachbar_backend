8558084060b542ed2e6aaa2afd05661c8d1e67ba
darshit.dobariya@cybercom.in <darshit.dobariya@cybercom.in>
2025-03-19 18:44:29 +0530

    fix uri

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 const connectDB = async () => {
   try {
     console.log("MongoDB connecting...",`${process.env.DB_URL}/${process.env.DB_NAME}`);
-    await connect(`${process.env.DB_URL}/${process.env.DB_NAME}`);
+    await connect(`${process.env.DB_URL}`);
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("MongoDB connection error:", err);
