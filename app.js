8e3d738316b64f7f3500a98220ab69123cf5b6d8
darshit.dobariya@cybercom.in <darshit.dobariya@cybercom.in>
2025-03-19 18:01:03 +0530

    add cors

--- app.js
+++ app.js
@@ -16,18 +16,21 @@ const app = express();
 //     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
 //   })
 // );
-app.use(
-  cors({
-    origin: "*", // Allows all origins (change this to specific origins if needed)
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-    credentials: true // Allow credentials (cookies, authorization headers, etc.)
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*", // Allows all origins (change this to specific origins if needed)
+//     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+//     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+//     credentials: true // Allow credentials (cookies, authorization headers, etc.)
+//   })
+// );
+
+app.use(cors());
 
 app.use(json());
 
 connectDB();
+
 app.use("/api/images", express.static(path.join(__dirname, "public/uploads")));
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
